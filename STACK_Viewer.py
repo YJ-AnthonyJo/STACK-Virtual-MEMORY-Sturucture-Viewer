a44d805204d48b80df04a41e7816879382e921a1
yj.anthony <yj.anthonyjo@gmail.com>
2021-08-13 22:46:19 +0900

    [#5] set기본 기능 구현
    M	.gitignore
    A	Func.py
    M	STACK_Viewer.py

--- STACK_Viewer.py
+++ STACK_Viewer.py
@@ -1,6 +1,7 @@
 import re, os
 from typing import Dict
 import inspect
+from Func import *
 try:
     import readline
 except ImportError:
@@ -124,12 +125,7 @@ def push(CMD):
         vari = m.group(1).strip()
         
         #변수명 valid여부 체크
-        p = re.compile("^[a-zA-Z_$ㄱ-ㅎ가-힣ㅏ-ㅣ][\wㄱ-ㅎ가-힣ㅏ-ㅣ$]*$")
-        if not bool(p.match(vari)):
-            print(inspect.cleandoc("""Syntax Error! : Invalid Variable name
-                                    Check valid Variable name by help(varialbe_name)
-                                    if you evaluate your expression are rigth, please add issue AT 
-                                       https://github.com/YJ-AnthonyJo/STACK-Virtual-MEMORY-Sturucture-Viewer/issues/"""))
+        if not chk_valid_variable_name(vari):
             return
         
         data_string = m.group(2)
@@ -191,43 +187,238 @@ def push(CMD):
 
 def set_(CMD):
     '''
-    \{\} : 실제로 들어가는 string에서의 {}을 의미.
-    ###set###
-    
-    #1 variable : $사용하기.
-        set {$vari_name}={data || $vari_name} {#length}
-        * {$vari_name} : 사용할 변수 이름. 공백 자동 제거(rstrip)
-        * {data || $vari_name} 
-            * data : 공백 없이 들어가야한다.
-            * $vari_name : $다음에 들어가는 이름(공백 자동 제거(rstrip))
-        * {#length}
-            * parm2 == $vari_name : 옮길 데이터 바이트 수. 생략시, 변수 전체 데이터
-            * parm2 == data : 할당할 데이터 크기, push와 동일한 작동기준.
+        \{\} : 실제로 들어가는 string에서의 {}을 의미.
+        ###set###
         
-    #2 istack : ebp기준의 상대주소 사용.
-        set \{{length}\}{#ebp esp}-{num}={data}
-        * {length} : 수식, 변수($사용) 허용.
-        * {#ebp} : 기본값이 ebp(생략가능), esp 사용가능.
-        * {num} : ebp로 부터 얼마나 떨어져있는지.
-        * {data} : 공백 없이 들어가야한다.
-        
-        명령 입력 후, 적용 결과를 임시로 보여주며(print) 진행할 것인지 확인.
+        #1 variable : $사용하기.
+            set {$vari_name}={data || $vari_name} {#length}
+            * {$vari_name} : 사용할 변수 이름. 공백 자동 제거(rstrip)
+            * {data || $vari_name} 
+                * data : 공백 없이 들어가야한다.
+                * $vari_name : $다음에 들어가는 이름(공백 자동 제거(rstrip))
+            * {#length}
+                * parm2 == $vari_name : 옮길 데이터 바이트 수. 생략시, 변수 전체 데이터
+                * parm2 == data : 할당할 데이터 크기, push와 동일한 작동기준.
+            
+        #2 istack : ebp기준의 상대주소 사용.
+            set \{{length}\}{#ebp esp}-{num}={data}
+            * {length} : 수식, 변수($사용) 허용.
+            * {#ebp} : 기본값이 ebp(생략가능), esp 사용가능.
+            * {num} : ebp로 부터 얼마나 떨어져있는지.
+            * {data} : 공백 없이 들어가야한다.
+            
+            명령 입력 후, 적용 결과를 임시로 보여주며(print) 진행할 것인지 확인.
     '''
-    
-    p_chk_vari = re.compile("^[a-zA-Z_$ㄱ-ㅎ가-힣ㅏ-ㅣ][\wㄱ-ㅎ가-힣ㅏ-ㅣ$]*$")
-    
-    p = re.compile(r'set (\$.+)= *(\$.+)') # for $vari1 = $vari2 {#length}
+    "set new"
+    # set new $vari1 = $vari2 {#length}
+    p = re.compile(r'set +new +\$(.+)= *\$(.+)') 
     m = p.match(CMD)
     if bool(m): 
-        vari1, vari2 = [m.group(i).rstrip()[1:] for i in [1,2]] # $variables_name -> variables_name
+        vari1, vari2 = [m.group(i).rstrip() for i in [1,2]]
+        
+        p = re.compile(r'(.+) +(\d+)')
+        m = p.match(vari2)
+        if bool(m):
+            vari2 = m.group(1)
+            byte = int(m.group(2))
+        else:
+            byte = len(VARIABLES[vari2])
+        
+        if not chk_valid_variable_name(vari1, vari2): return #변수명 확인.
+        
+        
+        if vari1 in VARIABLES: #기존에 존재시.
+            print("This Variable name is already exist, please use other.")
+            return
+        else:
+            VARIABLES[vari1] = VARIABLES[vari2][:byte]
         return
     
-    p = re.compile(r'set (\$.+)=(.+)') # for $vari1 = data #data는 따옴표안에 공간 이름에 담는 것 따로 두기.
+    # set new $vari1 = 'data' {#length}
+    p = re.compile(r'set +new +\$(.+)= *[\'\"](.*)[\'\"] *(\d*)') 
     m = p.match(CMD)
     if bool(m):
-        vari1 = m.group(1).rstrip()[1:]; data = m.group(2) #$vari_name -> vari_name && data
+        vari1 = m.group(1).rstrip() 
+        if not chk_valid_variable_name(vari1): return
+        data = m.group(2)
+        byte = m.group(3)
+        byte = int(byte) if byte != '' else len(data)
+        
+        if vari1 in VARIABLES:
+            print("This Variable name is already exist, please use other.")
+            return
+        VARIABLES[vari1] = data[:byte]
         return
     
+    # set new #ebp-num = $vari2 {#length}
+    p = re.compile(r'set +new +(.*)([-+]\d+) *= *\$(.+)') 
+    m = p.match(CMD)
+    if bool(m): 
+        #기준 정하기. esp? ebp?
+        base = m.group(1)
+        base = base if base != '' else 'ebp'
+        
+        #상대주소 +num, -num
+        #지금은 +가 구현안되어있기에 -를 기준으로 구현.
+        relativeAddr = - int(m.group(2)) #+구현시 int앞의 -빼고 다른 조건 넣어서 처리해주기.
+        
+        
+        #변수명 받기.
+        vari = m.group(3)
+        p = re.compile(r'(.+) +(\d+)')
+        m = p.match(vari)
+        if bool(m):
+            vari = m.group(1)
+            byte = int(m.group(2))
+            pass
+        else:
+            byte = len(VARIABLES[vari])
+        if not chk_valid_variable_name(vari): return #변수명 확인.
+        
+        
+        
+        # [ ['{vari}', '{data}', {byte}], ...]
+        total = 0
+        for idx, tmp in enumerate(STACK):
+            _1, _2, stackByte = tmp
+            total += stackByte
+            if total == relativeAddr:
+                STACK.insert(idx, [vari, VARIABLES[vari][:byte], byte])
+        return
+        
+    # set new #ebp-num = 'data' {#length}
+    p = re.compile(r'set +new +(.*)([-+]\d+) *= *[\'\"](.*)[\'\"] *(\d*)') 
+    m = p.match(CMD)
+    if bool(m):
+        #기준 정하기. esp? ebp?
+        base = m.group(1)
+        base = base if base != '' else 'ebp'
+        
+        #상대주소 +num, -num
+        #지금은 +가 구현안되어있기에 -를 기준으로 구현.
+        relativeAddr = - int(m.group(2)) #+구현시 int앞의 -빼고 다른 조건 넣어서 처리해주기.
+        
+        #데이터 입력받기.
+        data = m.group(3)
+        
+        #byte받기.
+        byte = m.group(4)
+        byte = int(byte) if byte != '' else len(data)
+        
+        # [ ['{vari}', '{data}', {byte}], ...]
+        total = 0
+        for idx, tmp in enumerate(STACK):
+            _1, _2, stackByte = tmp
+            total += stackByte
+            if total == relativeAddr:
+                STACK.insert(idx, ['', data, byte])
+        return
+
+    "set"
+    # set $var1 = $var2 {#length}
+    p = re.compile(r'set +\$(.+)= *\$(.+)')
+    m = p.match(CMD)
+    if bool(m):
+        vari1, vari2 = [m.group(i).rstrip() for i in [1,2]]
+        
+        p = re.compile(r'(.+) +(\d+)')
+        m = p.match(vari2)
+        if bool(m):
+            vari2 = m.group(1)
+            byte = int(m.group(2))
+        else:
+            byte = len(VARIABLES[vari2])
+        
+        if not chk_valid_variable_name(vari1, vari2): return #변수명 확인.
+        
+        if vari1 not in VARIABLES:
+            print("This Variable name is not declared, please declare it first using set new")
+        else:
+            VARIABLES[vari1] = VARIABLES[vari2][:byte]            
+        return
+    
+    # set $var1 = 'data' {#length}
+    p = re.compile(r'set +\$(.+)= *[\'\"](.*)[\'\"] *(\d*)') 
+    m = p.match(CMD)
+    if bool(m):
+        vari1 = m.group(1).rstrip() 
+        if not chk_valid_variable_name(vari1): return
+        data = m.group(2)
+        byte = m.group(3)
+        byte = int(byte) if byte != '' else len(data)
+        
+        if vari1 not in VARIABLES:
+            print("This Variable name is not declared, please declare it first using set new")
+        else:
+            VARIABLES[vari1] = data[:byte]
+        return
+    
+    # set #ebp-num = $var1 {#length}
+    p = re.compile(r'set +(.*)([-+]\d+) *= *\$(.+)') 
+    m = p.match(CMD)
+    if bool(m):
+        #기준 정하기. esp? ebp?
+        base = m.group(1)
+        base = base if base != '' else 'ebp'
+        
+        #상대주소 +num, -num
+        #지금은 +가 구현안되어있기에 -를 기준으로 구현.
+        relativeAddr = - int(m.group(2)) #+구현시 int앞의 -빼고 다른 조건 넣어서 처리해주기.
+        
+        
+        #변수명 받기.
+        vari = m.group(3)
+        p = re.compile(r'(.+) +(\d+)')
+        m = p.match(vari)
+        if bool(m):
+            vari = m.group(1)
+            byte = int(m.group(2))
+            pass
+        else:
+            byte = len(VARIABLES[vari])
+        if not chk_valid_variable_name(vari): return #변수명 확인.
+        
+        
+        
+        # [ ['{vari}', '{data}', {byte}], ...]
+        total = 0
+        for idx, tmp in enumerate(STACK):
+            _1, _2, stackByte = tmp
+            total += stackByte
+            if total == relativeAddr:
+                STACK[idx] = [vari, VARIABLES[vari][:byte], byte]
+        return
+    
+    # set #ebp-num = 'data' {#length}
+    p = re.compile(r'set +(.*)([-+]\d+) *= *[\'\"](.*)[\'\"] *(\d*)') 
+    m = p.match(CMD)
+    if bool(m):
+        #기준 정하기. esp? ebp?
+        base = m.group(1)
+        base = base if base != '' else 'ebp'
+        
+        #상대주소 +num, -num
+        #지금은 +가 구현안되어있기에 -를 기준으로 구현.
+        relativeAddr = - int(m.group(2)) #+구현시 int앞의 -빼고 다른 조건 넣어서 처리해주기.
+        
+        #데이터 입력받기.
+        data = m.group(3)
+        
+        #byte받기.
+        byte = m.group(4)
+        byte = int(byte) if byte != '' else len(data)
+        
+        # [ ['{vari}', '{data}', {byte}], ...]
+        total = 0
+        for idx, tmp in enumerate(STACK):
+            _1, _2, stackByte = tmp
+            total += stackByte
+            if total == relativeAddr:
+                STACK[idx] =  ['', data, byte]
+        return
+    
+    
+
 
 
 STACK = list() # [ ['{vari}', '{data}', {byte}], ...]
@@ -292,13 +483,7 @@ while True:
                 m = p.match(CMD)
                 if bool(m):
                     vari = m.group(1).strip()
-                    p = re.compile("^[a-zA-Z_$ㄱ-ㅎ가-힣ㅏ-ㅣ][\wㄱ-ㅎ가-힣ㅏ-ㅣ$]*$")
-                    if not bool(p.match(vari)):
-                        print(inspect.cleandoc("""Syntax Error! : Invalid Variable name
-                                Check valid Variable name by help(varialbe_name)
-                                if you evaluate your expression are rigth, please add issue AT 
-                                https://github.com/YJ-AnthonyJo/STACK-Virtual-MEMORY-Sturucture-Viewer/issues/"""))
-                    else:
+                    if chk_valid_variable_name(vari):
                         print(vari, '=' , VARIABLES[vari])
                 else:
                     print("Err : Invalid Parameter")
